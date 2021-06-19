7ec1aedd42086dfdd6a8ec80d43dbf1fc219739b
ErikPDev <33035080+ErikPDev@users.noreply.github.com>
2021-06-19 23:44:03 +0300

    Added Signature Emoji support
    Added aside support
    Removed Useless code

--- PMMP-Emojis.js
+++ PMMP-Emojis.js
@@ -40,14 +40,12 @@
 
     const EmojisApi = "https://raw.githubusercontent.com/ErikPDev/PMMP-Emojis/main/emojis.json"; // Caching support, We don't want to call github every time.
     var DomInUse = false;
-    var InputInUse = false; // This is useless
     var PreviousEmoji = "";
     var PreviousConversation = "";
     if(localStorage.Emoji == undefined){
         console.log("Fetching data.");
         fetch(EmojisApi)
             .then(response => { response.text().then(response => {localStorage.setItem("Emoji",response)})});
-        // localStorage.setItem("Emoji",'{":smile:":"😊"}');
         console.log("Data fetched completed!");
     }else{
     console.log("Already fetched, using localstorage as data");
@@ -75,6 +73,13 @@
                 var EmojiCS = Emojis[EmojisShortCode[num]];
                 if(Emojis[EmojisShortCode[num]] == undefined){EmojiCS = "❓";}
                 listOfBlockquote[blockquote].innerHTML = listOfBlockquote[blockquote].innerHTML.replace(new RegExp(escapeRegExp(EmojisShortCode[num]), 'g'), EmojiCS);
+            }
+            const listOfaside = document.body.getElementsByTagName("aside");
+            for (var aside in listOfaside){
+                if(listOfaside[aside].innerHTML == undefined){continue;}
+                EmojiCS = Emojis[EmojisShortCode[num]];
+                if(Emojis[EmojisShortCode[num]] == undefined){EmojiCS = "❓";}
+                listOfaside[aside].innerHTML = listOfaside[aside].innerHTML.replace(new RegExp(escapeRegExp(EmojisShortCode[num]), 'g'), EmojiCS);
             }
         }
         DomInUse = false;
@@ -119,10 +124,8 @@
     function updateValue(e) {
         const IsEmojiRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
         if(!e.target.value.match(IsEmojiRegex)){ return; }
-            console.log("Matches!");
             e.target.value.match(IsEmojiRegex).forEach(EmojiID =>{
                 if(EmojiID == PreviousEmoji){e.target.value=PreviousConversation;return;}
-
                 e.target.value = e.target.value.replace(EmojiID, getKeyByValue(Emojis, EmojiID));
                 PreviousEmoji = EmojiID;
                 PreviousConversation = e.target.value;
